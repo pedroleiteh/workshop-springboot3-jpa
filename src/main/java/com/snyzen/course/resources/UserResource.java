bbeabc77e7d1017c272628bfff027ad8c5fc0b9f
Pedro Leite <contato.pedro77@gmail.com>
2022-12-23 17:56:10 -0300

    implementing User entity and resource

---
+++ src/main/java/com/snyzen/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.snyzen.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.snyzen.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Pedro", "pedro@mail.com", "11945713799", "333333");
+		return ResponseEntity.ok().body(u);
+	}
+}
